f4bf982abdddc0b67658a518c2ce24b9830262df
aliserhan315 <aliserhan315@gmail.com>
2025-07-12 02:44:47 +0300

    created the navbar and buttons

---
+++ time-port-frontend/src/Components/Buttons.jsx
@@ -0,0 +1,17 @@
+import '../Styles/Buttons.css';
+
+export const BUTTON_TYPE_CLASSES = {
+  base: 'base',
+  google: 'google-sign-in',
+  inverted: 'inverted',
+};
+
+const Button = ({ children, buttonType = BUTTON_TYPE_CLASSES.base ,...otherProps }) => {
+  return (
+    <button className={`button ${buttonType}`} {...otherProps}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ time-port-frontend/src/Components/NavBar.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import Button from './Buttons';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/NavBar.css'; 
+
+const Navbar = ({ isAuthenticated = false, Username, Userprofile }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="navbar">
+      <nav>
+        <div className='nav-logo'>
+        <div className="logo" onClick={() => navigate('/')}>
+          TimePort
+        </div>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/capsulewall">Capsule Wall</Link>
+        </div>
+        </div>
+      </nav>
+      
+
+      {isAuthenticated ? (
+        <div className="user-info">
+          <img src={Userprofile} alt='../../public/emptypfp.png' className="user-avatar" />
+          <span>{Username}</span>
+        </div>
+      ) : (
+        <div className="navbar-buttons">
+          <Button onClick={() => navigate('/login')}>Login</Button>
+          <Button buttonType="inverted" onClick={() => navigate('/signUp')}>Register</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
