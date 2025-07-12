b2aa84fffe108dd14cd0f4015503eaf52e96bdd5
aliserhan315 <aliserhan315@gmail.com>
2025-07-12 12:53:03 +0300

    created the hero section

---
+++ time-port-frontend/src/Components/HeroSection.jsx
@@ -0,0 +1,22 @@
+import Button from "./Buttons";
+import '../Styles/HeroSection.css';
+import savememories from '../assets/Savememories.jpg';
+
+const HeroSection = () => {
+  return (
+    <section className="Hero-Section">
+        <div className="Hero-section1">
+      <h1>TimePort Secure Protects Us</h1>
+      <h2><span className="highlight">Memories</span> In Digital Time</h2>
+      <p>
+        TimePort is a digital time capsule that lets you save documents, photos, videos, messages, and more
+        and revisit them anytime in the future. Secure and Timeless.
+      </p>
+      <Button>Learn more</Button>
+      </div>
+      <img src={savememories} alt="Save Memories" className="hero-image" />
+    </section>
+  );
+}
+
+export default HeroSection;
