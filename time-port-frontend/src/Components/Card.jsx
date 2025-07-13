844aa5e5bcaa7517073e91ecc7cf4624c2e8196c
aliserhan315 <aliserhan315@gmail.com>
2025-07-13 04:01:13 +0300

    created base card component and changed some styles

---
+++ time-port-frontend/src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import "../Styles/card.css"
+const Card = ({ Capsule }) => {
+    const { name, ownername ,ownerpfp,status,creationdate,activationdate,mood} = Capsule;
+
+    return (
+      <div className='cardcontainer'>
+       
+    <p>card here</p>
+      </div>
+    );
+  };
+
+export default Card
