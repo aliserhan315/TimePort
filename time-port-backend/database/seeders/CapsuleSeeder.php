ee0ce10c9d07e05e561055f72f4461dc2f10f9ef
aliserhan315 <aliserhan315@gmail.com>
2025-07-17 01:18:29 +0300

    made the api routs and intialts the controllers

---
+++ time-port-backend/database/seeders/CapsuleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Capsule;
+
+class CapsuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Capsule::factory(30) ->create();
+    }
+}
